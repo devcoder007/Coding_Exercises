f4615369c9d30f7f2369f7b78989e8d90d428ce2
Gaushik0204 <sa03saz@gmail.com>
2019-08-16 05:02:58 -0400

    palin_updated

--- palin_temp.py
+++ palin_temp.py
@@ -1,43 +1,47 @@
 
 from collections import Counter
 while True:
+
     str = list(input("Please enter the string to check\n"))
-    c = Counter(list(str))
-    # print(c)
-    tt = []
-    for k in str:
-        if k not in tt:
-            tt.append(k)
-    # st = set(str)
-    temp_l=[]
-    for i in list(tt):
-        # print(c[i])
-        if c[i]in [j for j in range(0,22) if j%2 !=0]:#  == 1 or c[i]==3 or c[i]==5:
-            if str[len(str)//2] == i:
-                # print("ig",i)
-                pass
-            else:
-                # print("el")
-                temp_l.append(i)
-                # print(str)
-                str.remove(i)
-                # print(str)
-
-
-    # print(temp_l)
-    # print(str[::-1])
-    # print(str)
     if str == str[::-1]:
-        pass
+        print("".join(str), "Is already palindrome")
     else:
-        temp_l.append(str[len(str)//2-1])
-        # To make a palindrome
-        # temp = str[len(str)//2]
-        # str[len(str)//2] = str[len(str)//2-1]
-        # str[len(str)//2-1] = temp
+        c = Counter(list(str))
+        # print(c)
+        tt = []
+        for k in str:
+            if k not in tt:
+                tt.append(k)
+        # st = set(str)
+        temp_l=[]
+        for i in list(tt):
+            # print(c[i])
+            if c[i]in [j for j in range(0,22) if j%2 !=0]:#  == 1 or c[i]==3 or c[i]==5:
+                if str[len(str)//2] == i:
+                    # print("ig",i)
+                    pass
+                else:
+                    # print("el")
+                    temp_l.append(i)
+                    # print(str)
+                    str.remove(i)
+                    # print(str)
+
+
+        # print(temp_l)
+        # print(str[::-1])
         # print(str)
-    if len(str)<3:
-        print("not possible")
-    else:
-        print("Items to be deleted: ", temp_l)
-        print("Palindrome is:",str)
+        if str == str[::-1]:
+            pass
+        else:
+            temp_l.append(str[len(str)//2-1])
+            # To make a palindrome
+            # temp = str[len(str)//2]
+            # str[len(str)//2] = str[len(str)//2-1]
+            # str[len(str)//2-1] = temp
+            # print(str)
+        if len(str)<3:
+            print("not possible")
+        else:
+            print("Items to be deleted: ", temp_l)
+            print("Palindrome is:","".join(str))
